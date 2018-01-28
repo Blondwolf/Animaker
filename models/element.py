fef23ef3e018126c36b6b5a0c839a5e07051a23d
Thomas Lovis <thomas.lovis@gmail.com>
2018-01-28 14:25:05 +0100

    merge

---
+++ models/element.py
@@ -0,0 +1,24 @@
+from abc import ABCMeta, abstractmethod
+
+class Element(object):
+    ___metaclass__ = ABCMeta
+
+    @abstractmethod
+    def center(self):
+        pass
+
+    @abstractmethod
+    def draw(self):
+        pass
+		
+    @abstractmethod
+    def add_move(self, x, y):
+        pass
+		
+    @abstractmethod
+    def add_rotate(self, alpha):
+        pass
+
+    @abstractmethod
+    def move(self):
+        pass
